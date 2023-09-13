a6b988b289e8a81a9bcab04a65094cec9c9cd619
eric-kunin <eric.konin29@gmail.com>
2023-09-13 20:33:26 +0300

    added project 2

---
+++ My First JavaScript/js/message.js
@@ -0,0 +1,7 @@
+const message = () =>{
+    const name = "TT";
+    const age = 444444;
+    return `my name is ${name} and ${age}`;
+}
+
+export default message;
---
+++ My First JavaScript/js/person.js
@@ -0,0 +1,4 @@
+const name = "TT";
+const age = 444;
+
+export {name,age};
